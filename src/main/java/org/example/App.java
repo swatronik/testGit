4af48a576e18c20abe5288180255276ca6b9c608
Polyakov <d.polyakov@infomaximum.com>
2024-07-10 20:43:42 +0300

    remove space

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,8 +5,7 @@ package org.example;
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World" );
     }
 }
